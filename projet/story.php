b89100ab646cf6f7c160dc18731b54927250dc71
OceaneDelavauvre <32924191+OceaneDelavauvre@users.noreply.github.com>
2017-11-23 15:09:32 +0100

    Add files via upload

---
+++ projet/story.php
@@ -0,0 +1,86 @@
+<html>
+	<head>
+		<meta charset='utf-8'>
+		<link rel= "stylesheet" type="text/css" href="index.css">
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<link rel="stylesheet" href="css/projet.css"/>
+		<link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
+		<script type="application/javascript" src="js/jquery-2.1.1.min.js"></script>
+		<script type="application/javascript" src="js/bootstrap.min.js"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Histoire</title>
+		<style>
+		</style>
+	</head>
+	<body>
+		<div class="espace50">
+		</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<center>
+				<h1 id="color">Histoire:</h1>
+					<div class="espace">
+					</div>
+				
+						<div class="histoire">
+							Tu t'es perdu?<br>
+							
+						</div>
+					<div class="espace">
+					</div>
+						<div class="histoire">
+							Tu n'as qu'une solution pour surivre:<br>
+							Tu dois trouver la sortie!
+						</div>
+				
+				<div class="espace">
+				</div>
+				<div class="espace">
+				</div>
+						<div class="histoire">
+							Que la force de la forêt soit avec toi ...
+						</div>
+				<div class="espace">
+				</div>
+                  		
+					<a href="enigme1.php"><button type="submit" class="btn btn-default">Tu es prêt ?</button></a>
+				<div class="espace50">
+				</div>
+						<div class="histoire">
+							ou
+						</div>
+				<div class="espace50">
+				</div>
+					<!-- lancer une pop-up?-->
+                    <a href="menu.php"><button type="submit" class="btn btn-default">Tu trembles de peur ?</button></a><br><br>
+                    <!-- lancer une pop-up?-->
+                    <div class="espace">
+					</div>
+
+	</center></div>
+
+				
+
+		</div>
+	</div>
+	<div class="espace50">
+	</div>
+	<script type="text/javascript">
+        			$("#color").mouseover(function(){
+                	$("#color").css({"color":"#8B0000"});
+                });
+
+                	$("#color").mouseout(function(){
+                	$("#color").css({"color":"white"});
+            	});
+        </script>
+        
+			</div>
+		</div>				
+
+    	
+    </div>
+		
+</body>
+</html>
