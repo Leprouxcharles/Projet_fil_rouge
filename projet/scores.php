52b5fc27fab3643817a99d323987de89b7b22151
OceaneDelavauvre <32924191+OceaneDelavauvre@users.noreply.github.com>
2017-11-23 15:11:22 +0100

    Add files via upload

--- projet/scores.php
+++ projet/scores.php
@@ -18,7 +18,7 @@
 		<meta charset='utf-8'>
 		<link rel= "stylesheet" type="text/css" href="index.css">
 		<link rel="stylesheet" href="css/bootstrap.min.css"/>
-		<link rel="stylesheet" href="css/main.css"/>
+		
 		<link rel="stylesheet" href="css/projet.css"/>
 		<link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
 		<script type="application/javascript" src="js/jquery-2.1.1.min.js"></script>
@@ -29,36 +29,8 @@
 		</style>
 	</head>
 	<body>
-		<nav class="navbar navbar-inverse">
-			<div class="container-fluid">
-				<!--________________________container menu______________________________-->
-				<div class="row">
-					<div class="col-md-12">
-						<div class="navbar-header">
-							<button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#menu" title="Accueil"><img src="images/boutonmenu.png" height="35px"><!--bouton reflexive--></button>
-							<a class="navbar-brand" href="index.php"><img class="logohome" src="./images/logohome1.png" title="Accueil" height="70px"></a>
-						</div>
-						<div class="collapse navbar-collapse" id="menu">
-							<ul class="nav navbar-nav">
-								<a href="enigme1.php" title="Enigme 1">
-									<button  type="button" class="btn btn-primary btn-lg"><li>Enigme 1</li></button>
-								</a>
-								<a href="enigme2.php" title="Enigme 2">
-									<button  type="button" class="btn btn-primary btn-lg"><li>Enigme 2</li></button>
-								</a>
-								<a href="enigme3.php" title="Enigme 3">
-									<button  type="button" class="btn btn-primary btn-lg"><li>Enigme 3</li></button>
-								</a>
-								<a href="scores.php" title="Table des scores">
-									<button  type="button" class="btn btn-primary btn-lg"><li>Table des scores </li></button>
-								</a>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-
-	</nav>
+		<div class="espace50">
+		</div>
 
 	<!--________________________page______________________________-->
 
