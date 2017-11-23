f65e6914dfab66d5777c2f55670fd627a3f14b3b
Alo-Udev <32867580+Alo-Udev@users.noreply.github.com>
2017-11-23 12:54:58 +0100

    Add files via upload
    Nouvelle BDD 23/11/2017 suppression TScore

--- base de données/SBD_fil_rouge.sql
+++ base de données/SBD_fil_rouge.sql
@@ -6,7 +6,6 @@ USE Fil_rouge;
 
 DROP TABLE TPartie;
 DROP TABLE TJoueur;
-DROP TABLE TAvatar;
 DROP TABLE TScore;
 DROP TABLE TEnigme;
 
@@ -15,12 +14,6 @@ CREATE TABLE TEnigme(
 	id_enigme SMALLINT UNSIGNED auto_increment,
 	url VARCHAR(255) NOT NULL,
 	PRIMARY KEY (id_enigme)
-);
-
-CREATE TABLE TScore(
-	id_score SMALLINT UNSIGNED auto_increment,
-	score_joueur INT(10),
-	PRIMARY KEY (id_score)
 );
 
 CREATE TABLE TJoueur(
@@ -33,17 +26,15 @@ CREATE TABLE TJoueur(
 
 CREATE TABLE TPartie(
 	id_partie SMALLINT UNSIGNED auto_increment,
-    date_partie DATE,
+    score_joueur INT(10),
     id_joueur SMALLINT UNSIGNED NOT NULL,
-	id_score SMALLINT UNSIGNED NOT NULL,
 	id_enigme SMALLINT UNSIGNED NOT NULL,
     PRIMARY KEY (id_partie),
     CONSTRAINT Fk_id_jou FOREIGN KEY (id_joueur) REFERENCES TJoueur (id_joueur),
-	CONSTRAINT Fk_id_sco FOREIGN KEY (id_score) REFERENCES TScore (id_score),
 	CONSTRAINT Fk_id_enig FOREIGN KEY (id_enigme) REFERENCES TEnigme (id_enigme)
 );
 
-
+INSERT INTO TPartie (id_joueur, id_enigme) VALUE ('','','');
 INSERT INTO TScore (score_joueur) VALUES (2000), (200), (3000), (456782), (2345354), (57585847);
 
 SELECT * FROM TScore;
